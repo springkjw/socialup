1afea91cf049aaed9f8f58aa16bb1ea053d912b3
KwonJaewon <springkjw@gmail.com>
2016-09-19 10:28:58 +0900

    [release]

--- socialup/middleware.py
+++ socialup/middleware.py
@@ -24,6 +24,5 @@ class SiteMiddleware(object):
             current_site = Site.objects.get(domain=request.get_host())
         except Site.DoesNotExist:
             current_site = Site.objects.get(id=settings.DEFAULT_SITE_ID)
-
         request.current_site = current_site
         settings.SITE_ID = current_site.id
