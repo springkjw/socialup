7e03b4e4e0152b2fb1523c48517ee7fbf6b5ac22
KwonJaewon <springkjw@gmail.com>
2016-11-30 18:59:04 +0900

    home carousel

--- static/js/contact.js
+++ static/js/contact.js
@@ -1,9 +1,4 @@
 $(document).ready(function () {
-    $('.contact').scrollToFixed({
-        bottom: 0,
-        limit: $('footer').offset().top - 60
-    });
-
     $('.contact').hover(function () {
         $('.contact .contact-drop-down').stop(true, false, true).slideToggle(300);
     });
