b93dc3ca17284f093634566e8f61d67ba58d8e27
KimISeob <seobew@gmail.com>
2016-12-26 16:08:36 +0900

    implement contact with scroll
     - While scrolling, the position of contact box is fixed at bottom right.
     - When contact box reached footer, contact box's position is fixed above footer.

--- static/js/contact.js
+++ static/js/contact.js
@@ -1,5 +1,15 @@
 $(document).ready(function () {
-    $('.contact').hover(function () {
-        $('.contact .contact-drop-down').stop(true, false, true).slideToggle(300);
-    });
+	$('.contact').hover(function () {
+		$('.contact .contact-drop-down').stop(true, false, true).slideToggle(300);
+	});
+	var elementPosition = $('.contact').offset();
+	var temp =0;
+	$(window).scroll(function(){
+		temp = $(window).scrollTop() + $(window).height() - $(document).height() + 148;
+		if($(window).scrollTop() + $(window).height() > $(document).height() - 148){
+			$('.contact').css({'position':'fixed','bottom':temp, 'left':'87%', 'margin-bottom':'0'});
+		} else {
+			$('.contact').css({'position':'fixed','bottom':'0', 'left':'87%', 'margin-bottom':'1%'});
+		}
+	});
 });
