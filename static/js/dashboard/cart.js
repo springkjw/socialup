470a096b9b08d4d272cd53f87cf700cbec17a205
KimISeob <seobew@gmail.com>
2017-01-16 19:28:37 +0900

    dashboard/wishlist, cart part done

--- static/js/dashboard/cart.js
+++ static/js/dashboard/cart.js
@@ -19,3 +19,16 @@ $.fn.digits = function(){
         $(this).text( $(this).text().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,") );
     })
 };
+
+/* gauge part */
+$('.dashboard-cart-item').each(function(){
+    var color = $(this).attr('data-color');
+    $(this).find('.circle-wrapper .circle-list').eq(0).find('.circle-text').css({'background-color': color, 'color': 'white'});
+    var num = 1;
+    if(num == 1){
+        $(this).find('.circle-wrapper .circle-arrow').css({'position':'relative', 'left':'-15px', 'top':'38px', 'transform': 'rotate(18deg)'});
+    }
+    else if (num == 3){
+        $(this).find('.circle-wrapper .circle-arrow').css({'position':'relative', 'left':'26%', 'top':'-7px', 'transform': 'rotate(90deg)'});
+    }
+});
