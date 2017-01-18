d4b4e4ab713450349cfd9135714d35e8e3a27f1f
KimISeob <seobew@gmail.com>
2017-01-18 14:27:27 +0900

    account_change base css done, to do: form part

--- accounts/views.py
+++ accounts/views.py
@@ -21,6 +21,25 @@ def dashboard(request):
 
     return render(request, template, context)
 
+year_list = []
+for i in range(1930, 2003):
+    year_list.append(i)
+
+
+address_list=(
+    ("Seoul", "서울"),
+    ("Gyeonggi", "경기"),
+    ("Incheon", "인천"),
+    ("Gangwon", "강원"),
+    ("Gyeongnam", "경남"),
+    ("Gyeongbuk", "경북"),
+    ("Jeonbuk", "전북"),
+    ("Jeonnam", "전남"),
+    ("Jeju", "제주"),
+    ("Chungbuk", "충북"),
+    ("Chungnam", "충남"),
+    ("etc", "기타")
+)
 
 def change_info(request):
     user = MyUser.objects.get(id=request.user.id)
@@ -32,20 +51,26 @@ def change_info(request):
     })
 
     if request.method == "POST":
+        print('Post')
         form = ChangeForm(request.POST or None, request.FILES or None)
         if form.is_valid():
+            print('fomr_is_valid()')
             if form.cleaned_data['media'] != None:
                 user.media = form.cleaned_data['media']
+            print(form.cleaned_data)
             user.name = form.cleaned_data['name']
             user.phone = form.cleaned_data['phone']
             user.description = form.cleaned_data['description']
+            user.job = form.cleaned_data['job']
             user.save()
 
             return HttpResponseRedirect('/dashboard/change/')
 
     template = 'account/account_change.html'
     context = {
-        "form": form
+        "form": form,
+        "year_list": year_list,
+        "address_list":address_list,
     }
 
     return render(request, template, context)
