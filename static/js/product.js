171751c8640af73acfbdf6185ea6edc3e50dac97
KimISeob <seobew@gmail.com>
2017-02-03 17:35:34 +0900

    delete border-radius of cart modal button, reallocate the cart_modal's position

--- static/js/product.js
+++ static/js/product.js
@@ -248,12 +248,3 @@ function submit_form(){
 function set_input_value(selector, new_val){
     $(selector).val(new_val);
 }
-
-jQuery.fn.center = function () {
-    this.css("position","absolute");
-    this.css("top", Math.max(0, (($(window).height() - $(this).outerHeight()) / 2) +
-                                                $(window).scrollTop()) + "px");
-    this.css("left", Math.max(0, (($(window).width() - $(this).outerWidth()) / 2) +
-                                                $(window).scrollLeft()) + "px");
-    return this;
-}
