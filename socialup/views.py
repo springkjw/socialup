190d1dc1607bc56932a59abae8b1ed1787179184
KimISeob <seobew@gmail.com>
2017-02-08 19:08:01 +0900

    category header with border-bottom

--- socialup/views.py
+++ socialup/views.py
@@ -73,11 +73,9 @@ def product_category(request, category):
         product_ids_by_tags = []
         for product in products_by_tags:
             product_ids_by_tags.append(product.id)
-
         data = {
             "product_ids_by_tags": product_ids_by_tags,
             "checked_tags": checked_tags,
-            "category_type":category
         }
 
         return HttpResponse(json.dumps(data), content_type='application/json')
