2aac677d34b44f0e38376a75109ae11fd382d738
KimISeob <seobew@gmail.com>
2017-02-15 14:22:13 +0900

    http:// -> https:// (issue #200)

--- markets/forms.py
+++ markets/forms.py
@@ -40,7 +40,7 @@ class ProductForm(forms.ModelForm):
         )
         self.fields['sns_type'] = forms.ChoiceField(widget=forms.RadioSelect(attrs={'type': 'radio'}), choices=sns_type_list)
         self.fields['sns_additional_info'] = forms.ChoiceField(widget=forms.RadioSelect, choices=sns_additional_info_list, initial='individual')
-        self.fields['sns_url'] = forms.CharField(widget=forms.URLInput(attrs={'placeholder': 'url을 입력해주세요.', 'required':'true', 'value':'http://'}))
+        self.fields['sns_url'] = forms.CharField(widget=forms.URLInput(attrs={'placeholder': 'url을 입력해주세요.', 'required':'true', 'value':'https://'}))
         self.fields['sex'] = forms.ChoiceField(widget=forms.RadioSelect, choices=(('male','남자'),('female','여자')), initial='female')
         self.fields['message_to_buyer'] = forms.CharField(widget=forms.Textarea(
                 attrs={'placeholder':'포스팅불가능 업종, A/S규정, 진행방법등','cols':'95'
