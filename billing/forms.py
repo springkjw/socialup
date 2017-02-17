e482913afb301f115656b357b94e5260b5aad2d0
KangHyeongMin <humn9136@gmail.com>
2017-02-17 23:15:59 +0900

    load previous review (issue #235)

--- billing/forms.py
+++ billing/forms.py
@@ -11,7 +11,7 @@ class ReviewForm(forms.ModelForm):
         self.fields['contents_satisfy'] = forms.ChoiceField(widget=forms.RadioSelect(attrs={'type': 'radio'}), choices=SATISFY_CHOICES)
         self.fields['ad_satisfy'] = forms.ChoiceField(widget=forms.RadioSelect(attrs={'type': 'radio'}), choices=SATISFY_CHOICES)
         self.fields['kind_satisfy'] = forms.ChoiceField(widget=forms.RadioSelect(attrs={'type': 'radio'}), choices=SATISFY_CHOICES)
-        self.fields['rating'] = forms.ChoiceField(widget=forms.RadioSelect(attrs={'type': 'radio'}), choices=(('0.5',0.5),('1',1),('1.5',1.5),('2',2),('2.5',2.5),('3',3),('3.5',3.5),('4',4),('4.5',4.5),('5',5)))
+        self.fields['rating'] = forms.ChoiceField(widget=forms.RadioSelect(attrs={'type': 'radio'}), choices=(('0.50',0.5),('1.00',1),('1.50',1.5),('2.00',2),('2.50',2.5),('3.00',3),('3.50',3.5),('4.00',4),('4.50',4.5),('5.00',5)))
         self.fields['review'] = forms.CharField(widget=forms.Textarea(attrs={'required': True, 'rows':5, 'cols':45}))
 
     class Meta:
