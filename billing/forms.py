8cee2203b4e1eef95dc3b6956d28eb57f099e5a0
KangHyeongMin <humn9136@gmail.com>
2017-02-21 17:57:25 +0900

    review requirements fix (issue #290)

--- billing/forms.py
+++ billing/forms.py
@@ -8,9 +8,9 @@ class ReviewForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(ReviewForm, self).__init__(*args, **kwargs)
 
-        self.fields['contents_satisfy'] = forms.ChoiceField(widget=forms.RadioSelect(attrs={'type': 'radio'}), choices=SATISFY_CHOICES)
-        self.fields['ad_satisfy'] = forms.ChoiceField(widget=forms.RadioSelect(attrs={'type': 'radio'}), choices=SATISFY_CHOICES)
-        self.fields['kind_satisfy'] = forms.ChoiceField(widget=forms.RadioSelect(attrs={'type': 'radio'}), choices=SATISFY_CHOICES)
+        self.fields['contents_satisfy'] = forms.ChoiceField(widget=forms.RadioSelect(attrs={'required': True, 'type': 'radio'}), choices=SATISFY_CHOICES)
+        self.fields['ad_satisfy'] = forms.ChoiceField(widget=forms.RadioSelect(attrs={'required': True, 'type': 'radio'}), choices=SATISFY_CHOICES)
+        self.fields['kind_satisfy'] = forms.ChoiceField(widget=forms.RadioSelect(attrs={'required': True, 'type': 'radio'}), choices=SATISFY_CHOICES)
         self.fields['rating'] = forms.ChoiceField(widget=forms.RadioSelect(attrs={'type': 'radio'}), choices=(('0.50',0.5),('1.00',1),('1.50',1.5),('2.00',2),('2.50',2.5),('3.00',3),('3.50',3.5),('4.00',4),('4.50',4.5),('5.00',5)))
         self.fields['review'] = forms.CharField(widget=forms.Textarea(attrs={'required': True, 'rows':5, 'cols':45, 'placeholder': '한글 기준 최소 5자 이상', 'minlength':5}))
 
